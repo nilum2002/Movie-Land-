800af3f3ceed124e6cd8d4b29c2fa21b245c6d41
Nilum <sachithyanilum@gmail.com>
2025-07-13 18:16:41 +0530

    Search Problem Solved

--- src/App.js
+++ src/App.js
@@ -11,16 +11,35 @@ const API_URL = "https://www.omdbapi.com?apikey=bfa8425b";
 function App() {
   const [Movies, SetMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState(" ");
+  const [deBouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
     const searchMovies = async(title) =>{
+
+      if (title.trim() === ""){
+        SetMovies([]);
+      }
       const response = await fetch(`${API_URL}&s=${title}`);
       const data = await response.json();
       console.log(data.Search);
       SetMovies(data.Search);
     }
-  useEffect(() =>{
-    searchMovies(searchTerm);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter"){
+      searchMovies(searchTerm);
+    }
+  }
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+    }, 500);
+    return () => {
+      clearTimeout(handler);
+    };
   }, [searchTerm])
+  useEffect(() =>{
+    searchMovies(deBouncedSearchTerm);
+  }, [deBouncedSearchTerm])
     
 
     return (
@@ -28,7 +47,13 @@ function App() {
       <h1>Movie Land</h1>
 
       <div className = "search">
-        <input placeholder="Search For Movies" value = {searchTerm} onChange={(e) => setSearchTerm(e.target.value) }></input>
+        <input 
+          placeholder="Search For Movies" 
+          value = {searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value) } 
+          onKeyDown ={handleKeyPress} >
+
+        </input>
         <img src = {searchIcon} alt = "Search" onClick={() => searchMovies(searchTerm)}></img>
       </div>
       <div className = "container">
